b8369775fe5edadebbaa58dfa8e3f79cafbc240a
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-12-19 20:41:33 +0600

    Changed to remote url

--- src/redux/thunk/blogs.js
+++ src/redux/thunk/blogs.js
@@ -3,7 +3,7 @@ import { getBlogs } from "../actions/blogActions";
 
 export const loadBlogs = () => {
     return async (dispatch) => {
-        const blogs = await axios.get("http://localhost:5000/api/blogs");
+        const blogs = await axios.get("https://contentmanagementapplication.onrender.com/api/blogs");
         if (blogs.status === 200) {
             dispatch(getBlogs(blogs.data.blogs));
         }
